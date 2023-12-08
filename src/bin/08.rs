aa8c818829ed9a4400a730db17edaa5eaa666c3b
Rohan Godha <rgodha24@brophybroncos.org>
2023-12-07 22:56:42 -0700

    day 8

---
+++ src/bin/08.rs
@@ -0,0 +1,123 @@
+advent_of_code::solution!(8);
+use std::collections::BTreeMap;
+
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let (dirs, grid) = parse(input);
+
+    Some(time_to("AAA", &dirs, &grid, |s| s == "ZZZ"))
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let (dirs, grid) = parse(input);
+
+    let curr = grid
+        .keys()
+        .filter(|&k| k.ends_with("A"))
+        .map(|&s| time_to(s, &dirs, &grid, |s| s.ends_with("Z")))
+        .collect_vec();
+
+    Some(lcm(curr))
+}
+
+enum Dir {
+    R,
+    L,
+}
+
+fn time_to(
+    start: &str,
+    dirs: &[Dir],
+    grid: &BTreeMap<&str, (&str, &str)>,
+    is_done: impl Fn(&str) -> bool,
+) -> usize {
+    let mut curr = start;
+
+    for i in 0usize.. {
+        let d = &dirs[i.rem_euclid(dirs.len())];
+
+        let (left, right) = grid.get(curr).unwrap();
+
+        let next = match d {
+            Dir::R => right,
+            Dir::L => left,
+        };
+
+        if is_done(*next) {
+            return i + 1;
+        }
+
+        curr = next;
+    }
+
+    panic!("no path to Z");
+}
+
+fn parse(input: &str) -> (Vec<Dir>, BTreeMap<&str, (&str, &str)>) {
+    let mut lines = input.lines();
+    let dirs = lines
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| match c {
+            'R' => Dir::R,
+            'L' => Dir::L,
+            _ => unreachable!(),
+        })
+        .collect_vec();
+
+    // whitespace
+    lines.next();
+
+    let grid: BTreeMap<&str, (&str, &str)> = lines
+        .map(|l| {
+            let (start, rest) = l.split_once(" = ").unwrap();
+            let (left, right) = rest.split_once(", ").unwrap();
+            let left = left.trim_start_matches("(");
+            let right = right.trim_end_matches(")");
+
+            (start, (left, right))
+        })
+        .collect();
+
+    (dirs, grid)
+}
+
+fn lcm(data: Vec<usize>) -> usize {
+    // love that this needs a whole ass crate
+    data.into_iter().fold(1, |acc, x| num::integer::lcm(acc, x))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = r#"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"#;
+        let result = part_one(input);
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+
+    // this is embarrasing
+    #[test]
+    fn test_lcm() {
+        assert_eq!(lcm(vec![2, 3, 4]), 12);
+        assert_eq!(lcm(vec![2, 3, 4, 5]), 60);
+    }
+}
